adcbb3b398ce2a2420debc600ad2697aee5058b7
alice <xuetingz1@student.unimelb.edu.au>
2024-05-05 18:13:23 +1000

    publication ui

--- my-remote-web/src/pages/Publication/index.jsx
+++ my-remote-web/src/pages/Publication/index.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import "./style.less";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const Publication = () => {
+
+  const years = [];
+  for (let year = 2024; year >= 2013; year--) {
+    years.push(
+      <div className="publication-container">
+        <FontAwesomeIcon icon={faPlus} className="publication-icon" />
+        <span>{year}</span>
+        <div className="divider"></div>
+      </div>
+    );
+  }
+
   return (
-    <div>
+    <div className="publication">
       <h1>Publication</h1>
+      <div className="divider"></div>
+        {years}
     </div>
   )
 }
