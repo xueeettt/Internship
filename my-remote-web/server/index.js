435c233ccaba330ed4e36ef2f7761fef12c7dc67
alice <xuetingz1@student.unimelb.edu.au>
2024-06-09 17:02:52 +1000

    fix

--- my-remote-web/server/index.js
+++ my-remote-web/server/index.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const router = require('./router');
 
-
 const app = express();
 
 app.use(cors({
-  origin: ["http://13.239.31.245:3000","http://remod.online"]
+  origin: ["http://13.239.31.245:3000", "http://remod.online"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
+
 app.use(express.json());
 app.use("/api", router);
 
