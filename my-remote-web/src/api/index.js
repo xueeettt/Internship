02f385d15b66622256d228def2617d5109f80426
alice <xuetingz1@student.unimelb.edu.au>
2024-06-09 17:53:57 +1000

    delete console

--- my-remote-web/src/api/index.js
+++ my-remote-web/src/api/index.js
@@ -12,62 +12,22 @@ const base = {
 const api = {
   getTeamBio() {
     return axios.get(base.baseUrl + base.teamBio, { withCredentials: true })
-      .then(response => {
-        console.log('Team Bio response:', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching team bio:', error.response ? error.response.data : error.message);
-        throw error;
-      });
   },
 
   getResearchInfo() {
     return axios.get(base.baseUrl + base.researchContent, { withCredentials: true })
-      .then(response => {
-        console.log('Research Info response:', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching research info:', error.response ? error.response.data : error.message);
-        throw error;
-      });
   },
 
   sendContactForm(data) {
     return axios.post(base.baseUrl + base.sendEmail, data, { withCredentials: true })
-      .then(response => {
-        console.log('Contact Form response:', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('Failed to send contact form data:', error.response ? error.response.data : error.message);
-        throw error;
-      });
   },
 
   getNews() {
     return axios.get(base.baseUrl + base.news, { withCredentials: true })
-      .then(response => {
-        console.log('News response:', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching news:', error.response ? error.response.data : error.message);
-        throw error;
-      });
   },
 
   getPapers() {
     return axios.get(base.baseUrl + base.papers, { withCredentials: true })
-      .then(response => {
-        console.log('Papers response:', response.data);
-        return response.data;
-      })
-      .catch(error => {
-        console.error('Error fetching papers:', error.response ? error.response.data : error.message);
-        throw error;
-      });
   }
 };
 
